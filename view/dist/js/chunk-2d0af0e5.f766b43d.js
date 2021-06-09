9b1ddff3f25665b2cfedfd6f8c5d3be853e61d59
Aldo Bernardes Maciel <aldo-maciel@hotmail.com>
2021-06-09 02:21:46 -0300

    feat(app): rename commands to deploy on heroku

---
+++ view/dist/js/chunk-2d0af0e5.f766b43d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af0e5"],{"0d61":function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"text-center"},[n("div",{staticClass:"not-found-text"},[n("div",{staticClass:"number"},[n("h1",[t._v("ooopss!!!!")]),n("h2",[t._v("NOT FOUND - 404")])])])])}],c=n("2877"),i={},l=Object(c["a"])(i,s,a,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0af0e5.f766b43d.js.map
